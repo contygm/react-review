edfa9a02f10baca81e5ac64222a0626d33566cea
contygm <contygm@gmail.com>
2022-02-04 15:18:30 -0800

    ab: added basic card component, hardcoded

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 import Navbar from "./components/Navbar"
 import './index.css';
 
@@ -8,6 +9,7 @@ export default function App() {
         <div>
           <Navbar />
           <Hero />
+          <Card />
         </div>
     )
 }
