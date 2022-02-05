bf09ee7c9a58360b47cf9c04b0dcf8d5de411060
contygm <contygm@gmail.com>
2022-02-05 13:58:05 -0800

    tenzies: view setup, no functionality

---
+++ tenzies/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Die from "./components/Die"
+import './index.css';
+
+export default function App() {
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
+    )
+}
