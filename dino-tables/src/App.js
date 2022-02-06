27857daedeb3e42b6db7d7b3028e46f1ae9f9026
contygm <contygm@gmail.com>
2022-02-06 14:27:24 -0800

    dino-tables: basic react table displays all data categroies -> viewAll

--- dino-tables/src/App.js
+++ dino-tables/src/App.js
@@ -1,9 +1,25 @@
 import React from "react"
+import data from './fs-sample-data.json';
+import Table from "./components/Table";
 import './index.css';
 
 
 export default function App() {
+    const [dino, setDino] = React.useState(data.dinosaurs);
+    const [loot, setLoot] = React.useState(data.loot);
+    const [habitat, setHabitat] = React.useState(data.habitats);
+
+    
     return (
-        <h1>App component</h1>
+        <div>
+            <h2>Loot: {loot.length}</h2>
+            <h2>Dino: {dino.length}</h2>
+            <h2>Habitat: {habitat.length}</h2>
+
+            <Table data={dino} />
+            <Table data={loot} />
+            <Table data={habitat} />
+        </div>
+        
     )
 }
