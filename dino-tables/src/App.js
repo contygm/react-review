42a0de8ddcee0ee8d37628cd51fa9015345aa49d
contygm <contygm@gmail.com>
2022-02-06 16:50:22 -0800

    other tables work, loot is an issue

--- dino-tables/src/App.js
+++ dino-tables/src/App.js
@@ -8,14 +8,14 @@ export default function App() {
     const [dino, setDino] = React.useState(data.dinosaurs);
     const [loot, setLoot] = React.useState(data.loot);
     const [habitat, setHabitat] = React.useState(data.habitats);
-
+    
+    
     
     return (
         <div>
             <h2>Loot: {loot.length}</h2>
             <h2>Dino: {dino.length}</h2>
             <h2>Habitat: {habitat.length}</h2>
-
             <Table data={dino} />
             <Table data={loot} />
             <Table data={habitat} />
--- dino-tables/src/components/Table.js
+++ dino-tables/src/components/Table.js
@@ -2,14 +2,60 @@ import React from "react"
 import { useTable } from 'react-table'
 
 export default function Table(props) {
-	const cols = Object.keys(props.data[0]).map(key => ({
-		Header: key.charAt(0).toUpperCase() + key.slice(1),
-        accessor: key
-	}));
+	
+
+	function getKeyArray(dataArray) {
+        return Object.keys(dataArray[0]).map(key => {
+			if(typeof dataArray[0][key] === 'object' && !Array.isArray(dataArray[0][key])) {
+				let nested = [key];
+				dataArray.forEach(loo => {
+					nested.push(...Object.keys(loo[key]))
+				})
+				
+				return Array.from(new Set(nested));
+			}
+
+        return key;
+    })}
+
+	function makeCols(keyArray)  {
+		return keyArray.map(key => {
+			if(Array.isArray(key)) {
+				console.log("1 make cols ", key[0])
+				const header = key.shift();
+				return {
+					Header: header,
+					accessor: `${header}`,
+					columns: [
+						key.map(k => {
+							return {
+								Header: k.charAt(0).toUpperCase() + k.slice(1),
+								accessor: `${k}`,
+							}
+						})
+					]
+				}
+			} 
+			
+			return {
+				Header: key.charAt(0).toUpperCase() + key.slice(1),
+				accessor: key
+			}
+
+		});
+	}
+	// 	return keyArray.map(key => ({
+	// 		Header: key.charAt(0).toUpperCase() + key.slice(1),
+	// 		accessor: key
+	// 	}));
+	// }
+
 	const data = React.useMemo(() => props.data ,[]);
+	const colHeaderArray = getKeyArray(props.data);
+	const cols = makeCols(colHeaderArray);
+	
 	const columns = React.useMemo(() => cols,[]);
-	console.log(cols)
-	console.log(data)
+		console.log("2. ", columns)
 	const {
 		getTableProps,
 		getTableBodyProps,
